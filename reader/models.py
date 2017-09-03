98fbdefa7492a2dec32380c71ef453f441e82bdd
40huo <git@40huo.cn>
2017-09-03 21:30:30 +0800

    add intelligence model

--- reader/models.py
+++ reader/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
 
 # Create your models here.
+@python_2_unicode_compatible
+class Intelligence(models.Model):
+    """
+    威胁情报内容
+    """
+    title = models.CharField('标题', max_length=256)
+    url = models.CharField('地址', max_length=256)
+    content = models.TextField('内容', blank=True)
+    description = models.TextField('描述', blank=True)
+    create_time = models.DateTimeField('创建时间', auto_now_add=True)
+    last_modify_time = models.DateTimeField('修改时间', auto_now=True)
+
+    class Meta:
+        ordering = ['-create_time']
+        verbose_name = '威胁情报'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    """
+    分类
+    """
+    
--- reader/urls.py
+++ reader/urls.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 # __author__ = '40huo'
+
+from django.conf.urls import url
+from reader import views
+
+urlpatterns = [
+    url('^$', views.index, name='index'),
+]
