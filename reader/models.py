bf200d3806ddcf07bd1db3efab53f13084004ae3
40huo <git@40huo.cn>
2017-09-09 22:35:05 +0800

    models中添加标签与分类

--- reader/models.py
+++ reader/models.py
@@ -8,12 +8,20 @@ class Intelligence(models.Model):
     """
     威胁情报内容
     """
+    # 标题
     title = models.CharField('标题', max_length=256)
+    # URL 地址
     url = models.CharField('地址', max_length=256)
+    # 内容
     content = models.TextField('内容', blank=True)
-    description = models.TextField('描述', blank=True)
+    # 创建时间
     create_time = models.DateTimeField('创建时间', auto_now_add=True)
+    # 修改时间
     last_modify_time = models.DateTimeField('修改时间', auto_now=True)
+    # 分类
+    category = models.ForeignKey(IntelligenceCategory)
+    # 标签
+    tag = models.ManyToManyField(IntelligenceTag, blank=True)
 
     class Meta:
         ordering = ['-create_time']
@@ -24,13 +32,41 @@ class Intelligence(models.Model):
         return self.title
 
 
-class Category(models.Model):
+@python_2_unicode_compatible
+class IntelligenceCategory(models.Model):
     """
-    分类
+    情报分类
     """
-    pass
+    name = models.CharField('分类', max_length=64)
+
+    def __str__(self):
+        return self.name
 
 
+@python_2_unicode_compatible
+class IntelligenceTag(models.Model):
+    """
+    情报标签
+    """
+    name = models.CharField('标签', max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class ResourceCategory(models.Model):
+    """
+    情报源分类
+    """
+    # 分类名称
+    name = models.CharField('分类', max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
 class IntelligenceResource(models.Model):
     """
     情报源
@@ -39,11 +75,21 @@ class IntelligenceResource(models.Model):
     url = models.CharField('地址', max_length=256)
     # 情报源标题
     title = models.CharField('标题', max_length=256)
-    # 情报源描述
-    description = models.TextField('描述', blank=True)
     # 爬虫类型
     spider_type = models.IntegerField('爬虫类型')
     # 时间相关
     last_refresh_time = models.DateTimeField('上次刷新时间', auto_now=True)
     create_time = models.DateTimeField('创建时间', auto_now_add=True)
-    update_time = models.DateTimeField('修改时间', auto_now_add=True)
+    # 分类
+    category = models.ForeignKey(ResourceCategory)
+
+    def __str__(self):
+        return self.title
+
+
+@python_2_unicode_compatible
+class Spider(models.Model):
+    """
+    爬虫类型
+    """
+    name = models.CharField
