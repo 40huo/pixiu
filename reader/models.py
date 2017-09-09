efbcc1772abe150f0a3748d77b5df8e1f1ab50d6
40huo <git@40huo.cn>
2017-09-09 18:53:56 +0800

    添加情报源类

--- reader/models.py
+++ reader/models.py
@@ -28,4 +28,22 @@ class Category(models.Model):
     """
     分类
     """
-    
+    pass
+
+
+class IntelligenceResource(models.Model):
+    """
+    情报源
+    """
+    # 情报源地址
+    url = models.CharField('地址', max_length=256)
+    # 情报源标题
+    title = models.CharField('标题', max_length=256)
+    # 情报源描述
+    description = models.TextField('描述', blank=True)
+    # 爬虫类型
+    spider_type = models.IntegerField('爬虫类型')
+    # 时间相关
+    last_refresh_time = models.DateTimeField('上次刷新时间', auto_now=True)
+    create_time = models.DateTimeField('创建时间', auto_now_add=True)
+    update_time = models.DateTimeField('修改时间', auto_now_add=True)
