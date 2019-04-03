38e80d1b6e8ff8cd278750375a1be84dcada7697
40huo <git@40huo.cn>
2019-04-04 00:39:42 +0800

    添加静态文件支持

--- pixiu/settings.py
+++ pixiu/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.path('static')
+STATIC_ROOT = env.path('STATIC_ROOT')
+STATICFILES_DIRS = [
+    str(BASE_DIR.path('static'))
+]
 
 # Media files
 MEDIA_URL = '/media/'
