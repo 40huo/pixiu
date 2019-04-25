8911926750df1c0fc57248e844f9767cfd86aadb
40huo <git@40huo.cn>
2019-04-25 17:20:12 +0800

    修复deepmix爬虫

--- backend/spiders/deepmix.py
+++ backend/spiders/deepmix.py
@@ -53,41 +53,46 @@ class DeepMixSpider(BaseSpider):
         登录
         :return:
         """
+        # 第一次
         logger.debug(f'第一次请求 获取真正入口 {self.init_url}')
         init_req = self.session.get(self.init_url)
         match = re.search(r'url=(http://deepmix\w+\.onion)\">', init_req.text)
+        if '暗网欢迎您' in init_req.text:
+            return True
         if not match:
             logger.error(f'初始地址失效，返回内容 {init_req.text}')
             return False
-
         index_url = match.group(1)
+
+        # 第二次
         self.deepmix_index_url = index_url
         logger.debug(f'第二次请求 获取首页 {index_url}')
         index_req = self.session.get(index_url)
         match = re.search(r'url=(/\S+)\">', index_req.text)
+        if '暗网欢迎您' in index_req.text:
+            return True
         if not match:
             logger.error(f'首页请求失败，返回内容 {index_req.text}')
             return False
-
         pre_login_url = f'{index_url}{match.group(1)}'
-        logger.debug(f'第三次请求 跳转登录页 {pre_login_url}')
-        pre_login_req = self.session.get(pre_login_url)
-
-        login_url = f'{index_url}/ucp.php?mode=login'
-        logger.debug(f'第四次请求 登录POST {login_url}')
-
         match = re.search(r'autim=(\d+)', pre_login_url)
         if not match:
             logger.error(f'autim参数获取失败，返回内容 {pre_login_url}')
             return False
         autim = match.group(1)
 
+        # 第三次
+        logger.debug(f'第三次请求 跳转登录页 {pre_login_url}')
+        pre_login_req = self.session.get(pre_login_url)
         match = re.search(r'name=\"sid\" value=\"(\w+)\"', pre_login_req.text)
+        if '暗网欢迎您' in pre_login_req.text:
+            return True
         if not match:
             logger.error(f'sid参数获取失败，返回内容 {pre_login_req.text}')
             return False
         sid = match.group(1)
 
+        # 第四次
         post_data = {
             'sid': sid,
             'redirect': 'index.php',
@@ -96,6 +101,8 @@ class DeepMixSpider(BaseSpider):
             'username': self.username,
             'password': self.password
         }
+        login_url = f'{index_url}/ucp.php?mode=login'
+        logger.debug(f'第四次请求 登录POST {login_url}')
         login_req = self.session.post(login_url, data=post_data)
         if '暗网欢迎您' in login_req.text:
             return True
@@ -142,6 +149,7 @@ class DeepMixSpider(BaseSpider):
         if is_login:
             for zone in ('pay/user_area.php?q_ea_id=10001',):
                 data_list = await self.loop.run_in_executor(self.executor, self.parse, zone)
+                logger.info(f'抓取到 {len(data_list)} 条暗网数据')
                 for data in data_list:
                     await self.save(data=data)
 
