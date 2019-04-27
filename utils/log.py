2f28ed4588ba6ec0bdc37b37941e1487beea9a92
40huo <git@40huo.cn>
2019-04-27 21:14:00 +0800

    添加systemd日志支持

--- utils/log.py
+++ utils/log.py
@@ -17,7 +17,9 @@ class Logger(object):
         if not os.path.exists(log_dir):
             os.mkdir(log_dir)
 
-        log_format = "[%(asctime)s] [%(name)s] [line:%(lineno)s] [%(levelname)s] %(message)s"
+        default_log_format = "[%(asctime)s] [%(name)s] [line:%(lineno)s] [%(levelname)s] %(message)s"
+        systemd_log_format = "[%(name)s] [line:%(lineno)s] [%(levelname)s] %(message)s"
+        datetime_format = "%Y-%m-%d %H:%M:%S"
 
         logging_dict = {
             'version': 1,
@@ -31,12 +33,17 @@ class Logger(object):
             'formatters': {
                 'colored_console': {
                     '()': 'coloredlogs.ColoredFormatter',  # 使用自定义formatter的工厂类来实例化
-                    'format': log_format,
-                    'datefmt': '%Y-%m-%d %H:%M:%S'
+                    'format': default_log_format,
+                    'datefmt': datetime_format
                 },
-                'format_for_file': {
-                    'format': log_format,
-                    'datefmt': '%Y-%m-%d %H:%M:%S'}
+                'file': {
+                    'format': default_log_format,
+                    'datefmt': datetime_format
+                },
+                'systemd': {
+                    'format': systemd_log_format,
+                    'datefmt': datetime_format,
+                },
             },
             'handlers': {
                 'console': {
@@ -47,11 +54,16 @@ class Logger(object):
                 'file': {
                     'level': 'DEBUG',
                     'class': 'logging.handlers.RotatingFileHandler',
-                    'formatter': 'format_for_file',
+                    'formatter': 'file',
                     'filename': log_file,
                     'encoding': 'utf-8',
                     'maxBytes': 1024 * 1024 * 5,
                     'backupCount': 5
+                },
+                'systemd': {
+                    'level': 'DEBUG',
+                    'class': 'systemd.journal.JournalHandler',
+                    'formatter': 'systemd'
                 }
             },
         }
