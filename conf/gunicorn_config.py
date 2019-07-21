298f8adcdaa7991a0a058e3eec2d9de93d9ebdea
40huo <git@40huo.cn>
2019-07-21 19:16:05 +0800

    优化gunicorn

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -9,8 +9,8 @@ pid = '/run/gunicorn/pid'
 workers = multiprocessing.cpu_count() * 2 + 1
 
 # worker class
-# worker进程的类型，你可能需要手动安装：pip install gunicorn[tornado]
-worker_class = 'tornado'
+# worker进程的类型，你可能需要手动安装：pip install gunicorn[gevent]
+worker_class = 'gevent'
 
 # max_requests
 # 当worker进程每处理max_requests个请求后，会自动重启，如果为0，则表示永不重启
@@ -21,5 +21,5 @@ graceful_timeout = 3
 
 # remoteIP - 请求时间 请求行 状态码 返回值长度 "referer" "UA"
 accesslog = '-'
-access_log_format = '%(h)s %(l)s %(t)s %(r)s %(s)s %(b)s "%(f)s" "%(a)s"'
+access_log_format = '%({X-Real-IP}i)s %(l)s %(t)s %(r)s %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = 'info'
