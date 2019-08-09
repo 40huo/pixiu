0cb204512e11180ed79df4b58a1a38fb50fb89b6
40huo <git@40huo.cn>
2019-08-09 11:20:34 +0800

    拦截logging库的日志到loguru

--- utils/log.py
+++ utils/log.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 from loguru import logger
@@ -7,6 +8,17 @@ from pixiu.settings import core_env
 
 
 def init_log():
+    logging.getLogger('chardet').setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+    class InterceptHandler(logging.Handler):
+        def emit(self, record):
+            # Retrieve context where the logging call occurred, this happens to be in the 6th frame upward
+            logger_opt = logger.opt(depth=6, exception=record.exc_info)
+            logger_opt.log(record.levelname, record.getMessage())
+
+    logging.basicConfig(handlers=[InterceptHandler()], level=logging.DEBUG)
+
     logger.add(EventHandler(), format="{message}", level="ERROR")
     logger.add(BreadcrumbHandler(), format="{message}", level="ERROR")
     if core_env == "dev":
